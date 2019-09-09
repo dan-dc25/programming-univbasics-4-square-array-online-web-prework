a484bfff59c396ee139bf1b64bee8aa816d18814
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-09 02:19:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,8 @@ def square_array(array)
   array_1 = []
 
   while array[count] do
-    array_1 << array[count]
+    array_1 << array[count]**2
     puts array[count]
-    count **= 1
+    count += 1
   end
 end
